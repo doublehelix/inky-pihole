ef0c8c40548e384dba4f86f8f64168948b19a01d
James Price <jprice21@gmail.com>
2022-03-23 12:43:35 +1100

    v1.0 of advanced Pi-Hole eInk display.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # inky-pihole
 eInk Display for the PiHole
+
+Inspired by: https://github.com/neauoire/inky-hole
+
+Display the **number of blocked requests, and filtered traffic**, from [Pi-Hole](https://pi-hole.net), on [Pimoroni's Inky pHAT](https://github.com/pimoroni/inky-phat).
+
+<img src='https://raw.githubusercontent.com/doublehelix/inky-pihole/master/preview.jpg?v=1' width="400"/>
+
+- Setup **Pi-Hole**, follow the [installation instructions](https://learn.adafruit.com/pi-hole-ad-blocker-with-pi-zero-w/install-pi-hole).
+- Setup **Inky pHAT**, follow the [installation instructions](https://learn.pimoroni.com/tutorial/sandyj/getting-started-with-inky-phat).
+- Clone this repo on your [Raspberry Pi Zero W](https://www.raspberrypi.org/products/).
+
+## Reload automatically every 30 minutes
+
+Edit your `crontab` settings.
+
+```
+crontab -e
+```
+
+Add the following line:
+
+```
+*/30 * * * * python /home/pi/inky-pihole/main.py
+```
+where:
+*  `*/30`: is every 30 minutes.
+* `.../pi/inky-pihole/...`: **/pi/** is the username, and **/inky-pihole/** is the folder you cloned the repo into.
+
+30 minutes should be a non-obtrusive refresh time for most people. The display can flash quite a lot during updates, so refreshing more regularly is only recommended if you need closer monitoring.
+
+Cheers.
+
+## See also
+Remote debugging Python from Visual Studio Code:
+* https://www.linkedin.com/pulse/python-remote-debugging-visual-studio-code-raspberry-pi-mircea-dogaru/
---
+++ main.py
@@ -0,0 +1,92 @@
+import os
+import json
+from urllib.request import urlopen
+from PIL import Image, ImageFont, ImageDraw
+from inky.auto import auto
+# from gpiozero import CPUTemperature
+# from font_fredoka_one import FredokaOne
+
+# Set current directory
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+try:
+  cputemp = 0.0
+  # cputemp = CPUTemperature().temperature # THIS CRASHES INKYPHAT
+  # print(cputemp, type(cputemp))
+except Exception as ex:
+  print(ex)
+
+# get api data
+try:
+  f = urlopen('http://127.0.0.1/admin/api.php')
+  json_data_string = f.read()
+  pihole = json.loads(json_data_string)
+
+  # read JSON contents
+  clients = "{:,}".format(pihole['unique_clients'])
+  domains = "{:,}".format(pihole['domains_being_blocked'])
+  queries = "{:,}".format(pihole['dns_queries_today'])
+  blocked = "{:,}".format(pihole['ads_blocked_today'])
+  ratio   = "{:.1f}".format(round(pihole['ads_percentage_today'], 2))
+  blocked_full = blocked + " (" + ratio + "%)"
+
+  f.close()
+
+except Exception as ex:
+  print(ex)
+  clients = '--'
+  domains = '--'
+  queries = '--'
+  blocked = '--'
+  ratio   = '0.0'
+
+display = auto()
+# font = ImageFont.truetype(FredokaOne, 20)
+# ttf_bold = '/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf'
+font = ImageFont.truetype('./Signika-Bold.ttf', 22)
+# font.set_variation_by_name('Bold')
+# print(font.get_variation_names())
+
+# smallfont = ImageFont.truetype('./Signika-Medium.ttf', 14)
+smallfont = ImageFont.truetype('./Signika-Light.ttf', 14)
+
+# Load graphic
+img = Image.open("./logo.png")
+# Create canvas on the image we just loaded
+draw = ImageDraw.Draw(img)
+
+# Draw all text output
+def draw_text(position, prefix, text, suffix, colour):
+  # pW,pH = smallfont.getsize(prefix)
+  pW = smallfont.getlength(prefix)
+  # tW,tH = font.getsize(text)
+  tW = font.getlength(text)
+  w,h = position
+
+  if prefix:
+    draw.text((w, h+6), prefix, display.BLACK, smallfont)
+    w += pW + 5
+
+  if text:
+    draw.text((w, h), text, colour, font)
+    w += tW + 5
+
+  if suffix:
+    draw.text((w, h+6), suffix, display.BLACK, smallfont)
+# end draw_text
+
+draw.text((10,4), "blocked", display.BLACK, smallfont)
+draw_text((10,19), "", blocked_full, "", display.RED)
+draw_text((10,42), "of", queries, "requests", display.BLACK)
+
+draw_text((10, 75), "protecting", clients, "clients", display.BLACK)
+draw_text((10, 95), "@", domains, "domains", display.BLACK)
+
+# draw.text((10, 105), "core temp:", display.BLACK, smallfont)
+# draw_text((90,101), "core temp:", "{:.1f}".format(cputemp) + "°c", "", display.RED)
+
+img = img.rotate(180)
+
+display.set_border(display.WHITE)
+display.set_image(img)
+display.show()
