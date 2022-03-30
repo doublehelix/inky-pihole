1101f60c97c6528d7eceb7e17772be273e610506
James Price <jprice21@gmail.com>
2022-03-30 14:02:21 +1100

    Consolidated main/lcars/simple layout versions into one script Added Time-Zone switch Added API Hosts switch

--- README.md
+++ README.md
@@ -3,33 +3,74 @@ eInk Display for the PiHole
 
 Inspired by: https://github.com/neauoire/inky-hole
 
-Display the **number of blocked requests, and filtered traffic**, from [Pi-Hole](https://pi-hole.net), on [Pimoroni's Inky pHAT](https://github.com/pimoroni/inky-phat).
+Display statistics including the **number of blocked requests, and filtered traffic**, from [Pi-Hole](https://pi-hole.net), on [Pimoroni's Inky pHAT](https://github.com/pimoroni/inky-phat).
 
-<img src='https://raw.githubusercontent.com/doublehelix/inky-pihole/master/preview.jpg?v=1' width="400"/>
-
-LCARS Version:
-
-<img src='https://raw.githubusercontent.com/doublehelix/inky-pihole/master/preview_lcars.jpg?v=1' width="400"/>
+Default display / Simple display (using `--simple` option):  
+<img src='https://raw.githubusercontent.com/doublehelix/inky-pihole/master/preview.jpg?v=1' width="300"/> 
+<img src='https://raw.githubusercontent.com/doublehelix/inky-pihole/master/preview_simple.jpg?v=1' width="300"/>
 
 
-- Setup **Pi-Hole**, follow the [installation instructions](https://learn.adafruit.com/pi-hole-ad-blocker-with-pi-zero-w/install-pi-hole).
-- Setup **Inky pHAT**, follow the [installation instructions](https://learn.pimoroni.com/tutorial/sandyj/getting-started-with-inky-phat).
-- Clone this repo on your [Raspberry Pi Zero W](https://www.raspberrypi.org/products/).
+LCARS display (using `--lcars` option) / LCARS + Simple:  
+<img src='https://raw.githubusercontent.com/doublehelix/inky-pihole/master/preview_lcars.jpg?v=1' width="300"/> 
+<img src='https://raw.githubusercontent.com/doublehelix/inky-pihole/master/preview_lcars_simple.jpg?v=1' width="300"/>
 
-### Display Rotation
+## Installation
 
-*NB:* If you need to Rotate the image displayed on your eInk display, uncomment line 88
+1. Setup **Pi-Hole**, follow the [installation instructions](https://learn.adafruit.com/pi-hole-ad-blocker-with-pi-zero-w/install-pi-hole).
+2. Setup **Inky pHAT**, follow the [installation instructions](https://learn.pimoroni.com/tutorial/sandyj/getting-started-with-inky-phat).
+3. Clone this repo on your [Raspberry Pi Zero W](https://www.raspberrypi.org/products/).
+    ```
+    git clone https://github.com/doublehelix/inky-pihole.git
+    ```
+
+### Test your setup
+Executing the following command will run the script once and update the eInk display:
 ```
-img = img.rotate(180)
+python ./inky-pihole/main.py
 ```
-This allows you to mount your device with cable at the top instead of the bottom.
 
-## Roadmap
-* CPU Temerature Display
-  * This currently prevents the display from refreshing on python 3.10 / inky 1.6.2
+## Options
+All command line optins are optional and the default values when left out are detailed below.
+* `-a` , `--apihosts`  
+  A comma separated list of your Pi-Hole host names.  
+  e.g.: `--apihosts "192.168.1.10, 192.168.1.11"`  
+  *(Defaults to `"127.0.0.1"` if omitted)*  
+  *NOTE:* If you have more than one pi-hole (i.e. primary and secondary pi-hole DNS servers), the statistics will be consolidated.
+  This is because all clients can talk to one or both of the pi-hole servers.
+  Therefore, the total number of requests and blocked queries will be the **combined** values from all servers. Whereas the total number of blocked domains and clients will be the **greater** of the unique values from each server.
+
+* `--tz`  
+  Set the TimeZone of your pi-hole for date/time display purposes.  
+  e.g. `--tz "Australia/Melbourne"` or `--tz "Etc/GMT-10"`  
+  *(Defaults to `"UTC"` if omitted)*  
+
+* `-r` , `--rotate`  
+  Rotates the display 180 degrees.  
+  This allows you to mount your device with cable at the top instead of the bottom.
+
+* `-s` , `--simple`  
+  Show a simplified display with three large numeric values:
+    * +Number of Queries
+    * -Number Blocked
+    * Blocked Ratio %
+
+* `-l` , `--lcars`  
+  Show a Star Trek inspired LCARS console style display
+
+** *NOTE:* You can combine the `--lcars` and `--simple` options to display the LCARS background with the simple stats display (text will be all white).
+
+### Help options
+These functions will NOT update the display, and output help information to the console only.
+
+* `-h` , `--help`  
+  Display command line options
+
+* `--timezones`  
+  Display all available time zone names
 
 
-## Reload automatically every 30 minutes
+## Automatic / Scheduled update
+*NOTE:* The following assumes you're logged in a the `pi` user, and your home directory is `/home/pi`. (which was the folder you cloned the `inky-pihole` repository into)
 
 Edit your `crontab` settings.
 
@@ -41,21 +82,31 @@ Add the following line:
 ```
 */30 * * * * python /home/pi/inky-pihole/main.py
 ```
-Or, for the LCARS version:
+Or, add some command line options:
 ```
-*/30 * * * * python /home/pi/inky-pihole/lcars.py
+*/30 * * * * python /home/pi/inky-pihole/main.py --rotate --lcars --simple --apihosts "10.0.0.10,10.0.0.11" --tz "Australia/Melbourne"
 ```
-where:
-*  `*/30`: is every 30 minutes.
-* `.../pi/inky-pihole/...`: **/pi/** is the username, and **/inky-pihole/** is the folder you cloned the repo into.
 
 30 minutes should be a non-obtrusive refresh time for most people. The display can flash quite a lot during updates, so refreshing more regularly is only recommended if you need closer monitoring.
 
 Cheers.
 
+
+## Roadmap
+* CPU Temerature Display - This currently prevents the display from refreshing on python 3.10 / inky 1.6.2.
+
+
 ## See also
+
+Inky-phat:
+* Docs: http://docs.pimoroni.com/inkyphat/
+* GitHub: https://github.com/pimoroni/inky-phat
+
+Python Image Library (PIL):
+* https://pillow.readthedocs.io/en/stable/reference
+
 Signika Font:
 * https://fonts.google.com/specimen/Signika
 
 Remote debugging Python from Visual Studio Code:
-* https://www.linkedin.com/pulse/python-remote-debugging-visual-studio-code-raspberry-pi-mircea-dogaru/
+* https://www.linkedin.com/pulse/python-remote-debugging-visual-studio-code-raspberry-pi-mircea-dogaru
--- main.py
+++ main.py
@@ -1,92 +1,197 @@
-import os
+import argparse
+import distutils.util
 import json
+import os
+import pytz
+from datetime import datetime
 from urllib.request import urlopen
 from PIL import Image, ImageFont, ImageDraw
 from inky.auto import auto
 # from gpiozero import CPUTemperature
-# from font_fredoka_one import FredokaOne
 
 # Set current directory
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-try:
-  cputemp = 0.0
-  # cputemp = CPUTemperature().temperature # THIS CRASHES INKYPHAT
-  # print(cputemp, type(cputemp))
-except Exception as ex:
-  print(ex)
+# Initialize Command Line parser
+parser = argparse.ArgumentParser()
+parser.add_argument('-a', '--apihosts', help='List of comma separated API host names')
+parser.add_argument('-r', '--rotate', action='store_true', help='Rotate display 180 deg')
+parser.add_argument('-s', '--simple', action='store_true', help='Simple Statistics Display')
+parser.add_argument('--lcars', action='store_true', help='Use LCARS style display')
+parser.add_argument('--tz', help='Timezone (default: UTC)')
+# Helpers
+parser.add_argument('--timezones', action='store_true', help='Show all available TimeZone values')
 
-# get api data
-try:
-  f = urlopen('http://127.0.0.1/admin/api.php')
-  json_data_string = f.read()
-  pihole = json.loads(json_data_string)
+args = parser.parse_args()
+#print('Args:', args)
 
-  # read JSON contents
-  clients = "{:,}".format(pihole['unique_clients'])
-  domains = "{:,}".format(pihole['domains_being_blocked'])
-  queries = "{:,}".format(pihole['dns_queries_today'])
-  blocked = "{:,}".format(pihole['ads_blocked_today'])
-  ratio   = "{:.1f}".format(round(pihole['ads_percentage_today'], 2))
-  blocked_full = blocked + " (" + ratio + "%)"
+if (args.timezones):
+    for tz in pytz.all_timezones:
+        print(tz)
+    exit()
 
-  f.close()
-
-except Exception as ex:
-  print(ex)
-  clients = '--'
-  domains = '--'
-  queries = '--'
-  blocked = '--'
-  ratio   = '0.0'
-
+# Initialize Inky pHAT Display
 display = auto()
-# font = ImageFont.truetype(FredokaOne, 20)
-# ttf_bold = '/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf'
-font = ImageFont.truetype('./Signika-Bold.ttf', 22)
-# font.set_variation_by_name('Bold')
-# print(font.get_variation_names())
 
-# smallfont = ImageFont.truetype('./Signika-Medium.ttf', 14)
+# Colours depend on background image selected - RED doesn't show up wel on black BG.
+WHITE = display.WHITE
+BLACK = display.WHITE if args.lcars else display.BLACK
+RED = display.WHITE if args.lcars else display.RED
+
+# Get the current date/time based on TimeZone (if specified)
+tzLocal = pytz.timezone(args.tz or 'UTC')
+now = datetime.now(tzLocal)
+
+# Load True Type Fonts...
+font = ImageFont.truetype('./Signika-Bold.ttf', 22)
 smallfont = ImageFont.truetype('./Signika-Light.ttf', 14)
 
+# Initialize Data Object
+data = {'unique_clients': 0, 'domains_being_blocked': 0,
+        'dns_queries_today': 0, 'ads_blocked_today': 0}
+
+# Get Pi-Hole API data
+def get_data(host, combine=False):
+    try:
+        f = urlopen('http://' + host + '/admin/api.php')  # open API connection
+        json_data_string = f.read()                   # Read data
+        f.close()                                     # close connection
+
+        # print(json_data_string)
+        # Deserialize JSON to Python Object
+        tmp_data = json.loads(json_data_string)
+
+        # extract data
+        data['unique_clients'] = data['unique_clients'] + \
+            tmp_data['unique_clients'] if combine else max(
+                data['unique_clients'], tmp_data['unique_clients'])
+        data['domains_being_blocked'] = max(
+            tmp_data['domains_being_blocked'], data['domains_being_blocked'])
+        data['dns_queries_today'] += tmp_data['dns_queries_today']
+        data['ads_blocked_today'] += tmp_data['ads_blocked_today']
+
+    except Exception as ex:
+        print(ex)
+# end get_data()
+
+# Draw large text, with small prefix and suffix
+def draw_text(position, prefix, text, suffix, colour):
+    bg_colour = display.WHITE if args.lcars else display.BLACK
+    pw = smallfont.getlength(prefix)
+    tw = font.getlength(text)
+    w, h = position
+
+    if prefix:
+        draw.text((w, h+6), prefix, bg_colour, smallfont)
+        w += pw + 4
+
+    if text:
+        draw.text((w, h), text, colour, font)
+        w += tw + 4
+
+    if suffix:
+        draw.text((w, h+6), suffix, bg_colour, smallfont)
+# end draw_text()
+
+# Get X Position of text
+def get_x(text_width, margin):
+    RIGHT = 1; LEFT = 3;
+    dw = round(display.WIDTH - margin[LEFT] - margin[RIGHT])
+    x = round((dw - text_width) / 2) + margin[LEFT]
+    return x
+# end get_x()
+
+# Get Y Position of text
+def get_y(text_height, section, number_of_sections, margin):
+    TOP = 0; BOTTOM = 2;
+    dh = (display.HEIGHT - margin[TOP] - margin[BOTTOM]) / number_of_sections
+    y = round(dh * section) - text_height + margin[TOP]
+    return y
+# end get_y()
+
+# Get text position when "==simple" switch is used.
+def get_simple_position(text, font, row = 1, rows = 1):
+    # (top, right, bottom, left)
+    margin = (10,10,10,60) if args.lcars else (10,100,10,10)
+    w,h = font.getsize(text)
+    x = get_x(w, margin)
+    y = get_y(h, row, rows, margin)
+    return (x, y)
+
+# Get DATA and parse to strings
+api_host_names = (args.apihosts or "127.0.0.1").split(",")
+for host_item in api_host_names:
+    parts = host_item.split(":")
+    host = parts[0]
+    combine = bool(distutils.util.strtobool((parts[1] if len(parts) > 1 else "False") or "False"))
+    get_data(host, combine)  # primary
+# end for...
+
+fRatio = (data['ads_blocked_today'] / data['dns_queries_today']) * 100
+ratio = "{:.1f}".format(round(fRatio, 2))
+clients = "{:,}".format(data['unique_clients'])
+domains = "{:,}".format(data['domains_being_blocked'])
+queries = "{:,}".format(data['dns_queries_today'])
+blocked = "{:,}".format(data['ads_blocked_today'])
+blocked_full = blocked + " (" + ratio + "%)"
+
 # Load graphic
-img = Image.open("./logo.png")
+if (args.lcars):
+    display.set_border(display.BLACK)
+    img = Image.open("./lcars_black_red.png")
+else:
+    display.set_border(display.WHITE)
+    img = Image.open("./logo.png")
+
 # Create canvas on the image we just loaded
 draw = ImageDraw.Draw(img)
 
-# Draw all text output
-def draw_text(position, prefix, text, suffix, colour):
-  # pW,pH = smallfont.getsize(prefix)
-  pW = smallfont.getlength(prefix)
-  # tW,tH = font.getsize(text)
-  tW = font.getlength(text)
-  w,h = position
+# Render the display text...
+if (args.simple):
+    font = ImageFont.truetype('./Signika-Bold.ttf', 34)
 
-  if prefix:
-    draw.text((w, h+6), prefix, display.BLACK, smallfont)
-    w += pW + 5
+    pos = get_simple_position(queries, font, 1, 3)
+    draw.text(pos, queries, BLACK, font)
 
-  if text:
-    draw.text((w, h), text, colour, font)
-    w += tW + 5
+    pos = get_simple_position('-'+blocked, font, 2, 3)
+    draw.text(pos, '-'+blocked, RED, font)
 
-  if suffix:
-    draw.text((w, h+6), suffix, display.BLACK, smallfont)
-# end draw_text
+    pos = get_simple_position(ratio + '%', font, 3, 3)
+    draw.text((pos[0], pos[1]-5), ratio+'%', RED, font) # y-5 as '%' causes extra height
 
-draw.text((10,4), "blocked", display.BLACK, smallfont)
-draw_text((10,19), "", blocked_full, "", display.RED)
-draw_text((10,42), "of", queries, "requests", display.BLACK)
+    if (args.lcars): #lcars+simple
+        draw.text((2,45), now.strftime("%m.%d"), display.BLACK, smallfont)
+        draw.text((2,60), now.strftime("%H.%M"), display.BLACK, smallfont)
+        draw.text((15,100), clients, display.BLACK, smallfont)
 
-draw_text((10, 75), "protecting", clients, "clients", display.BLACK)
-draw_text((10, 95), "@", domains, "domains", display.BLACK)
+else:
+    if (args.lcars):
+        x = 60
+        draw_text((x, 5), now.strftime("%b-%d %H:%M"), "", "- blocked:", WHITE)
+        draw_text((x, 24), "", blocked_full, "", WHITE)
+        draw_text((x, 46), "of", queries, "requests", WHITE)
+        draw_text((x, 68), "protecting", clients, "clients", WHITE)
+        draw_text((x, 88), "@", domains, "domains", WHITE)
+    else:
+        x = 10
+        draw_text((x, -3), now.strftime("%b-%d %H:%M"), "", "- blocked:", BLACK)
+        draw_text((x, 19), "", blocked_full, "", RED)
+        draw_text((x, 42), "of", queries, "requests", BLACK)
+        draw_text((x, 75), "protecting", clients, "clients", BLACK)
+        draw_text((x, 95), "@", domains, "domains", BLACK)
 
-# draw.text((10, 105), "core temp:", display.BLACK, smallfont)
-# draw_text((90,101), "core temp:", "{:.1f}".format(cputemp) + "°c", "", display.RED)
+# CPU Temprature - Work in progress...
+try:
+    # Try getting CPU Temperature - FAILS ON "Pi Zero 2 W"
+    cputemp = 0.0
+    # cputemp = CPUTemperature().temperature # << THIS CRASHES INKYPHAT
+    # print(cputemp, type(cputemp))
+    # draw.text((10, 105), "core temp:", display.BLACK, smallfont)
+    # draw_text((90,101), "core temp:", "{:.1f}".format(cputemp) + "°c", "", display.RED)
+except Exception as ex:
+    print(ex)
 
-# img = img.rotate(180)
-
-display.set_border(display.WHITE)
+# RENDER!
+if (args.rotate): img = img.rotate(180) # image rotation switch - must happen after all text drawing.
 display.set_image(img)
 display.show()
